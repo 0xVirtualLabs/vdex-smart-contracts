92bab8b831aa5f79f463785e85f2447ab6a22cd1
hunter <truong.ngo@sotatek.com>
2024-08-16 14:07:09 +0700

    fix compile err

--- contracts/0xVault/Vault.sol
+++ contracts/0xVault/Vault.sol
@@ -486,7 +486,7 @@ contract Vault is
                     _checkLiquidatedPosition(dispute.positions[i], allFeeds, dispute.balances)
                 ) {
                     dispute.positions[i].quantity = 0;
-                    if (keccak256(abi.encodePacked(position.leverageType)) == keccak256(abi.encodePacked("cross"))) {
+                    if (keccak256(abi.encodePacked(dispute.positions[i].leverageType)) == keccak256(abi.encodePacked("cross"))) {
                         // update user balance if cross position is liquidated
                         for (uint256 k = 0; k < dispute.balances.length; k++) {
                             dispute.balances[k].balance = 0;
