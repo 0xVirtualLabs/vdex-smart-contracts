2bb6328d2277c79feb34c5021094d07446c85b8c
Sotatek-CuongTran <cuong.tran@sotatek.com>
2024-09-05 16:44:41 +0700

    audit: HAL-16 - SupraOracleDecoder.sol

--- contracts/0xVault/libs/SupraOracleDecoder.sol
+++ contracts/0xVault/libs/SupraOracleDecoder.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.24;
 
+/**
+ * @title SupraOracleDecoder
+ * @dev Library for decoding Oracle proofs in the Supra system.
+ */
 library SupraOracleDecoder {
+    /**
+     * @dev Struct representing a feed from a committee.
+     */
     struct CommitteeFeed {
         uint32 pair;
         uint128 price;
@@ -10,12 +17,18 @@ library SupraOracleDecoder {
         uint64 round;
     }
 
+    /**
+     * @dev Struct representing a feed from a committee with proof.
+     */
     struct CommitteeFeedWithProof {
         CommitteeFeed[] committee_feeds;
         bytes32[] proofs;
         bool[] flags;
     }
 
+    /**
+     * @dev Struct representing price details with committee information.
+     */
     struct PriceDetailsWithCommittee {
         uint64 committee_id;
         bytes32 root;
@@ -23,10 +36,18 @@ library SupraOracleDecoder {
         CommitteeFeedWithProof committee_data;
     }
 
+    /**
+     * @dev Struct representing an Oracle proof in version 2.
+     */
     struct OracleProofV2 {
         PriceDetailsWithCommittee[] data;
     }
 
+    /**
+     * @notice Decode the Oracle proof from bytes.
+     * @param _bytesProof The Oracle proof in bytes.
+     * @return Decoded Oracle proof in version 2.
+     */
     function decodeOracleProof(
         bytes calldata _bytesProof
     ) external pure returns (OracleProofV2 memory) {
