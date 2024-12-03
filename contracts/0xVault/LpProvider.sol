29ae842463601168f0c3720c703c92e8d57a0ae5
Sotatek-PhucNguyen4 <phuc.nguyen4@sotatek.com>
2024-12-03 17:54:35 +0700

    fix lp provider

--- contracts/0xVault/LpProvider.sol
+++ contracts/0xVault/LpProvider.sol
@@ -6,11 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {
-  ECDSAUpgradeable
-} from "@openzeppelin/contracts-upgradeable/utils/cryptography/.sol";
-
 import {IVault} from "./interfaces/IVault.sol";
 import {Crypto} from "./libs/Crypto.sol";
 
@@ -21,40 +18,20 @@ import {Crypto} from "./libs/Crypto.sol";
  * and manage liquidity for different tokens.
  */
 contract LpProvider is OwnableUpgradeable, ReentrancyGuardUpgradeable, EIP712Upgradeable {
-    // Constants
-    uint256 public constant NAV_DECIMALS = 18; // Used for precision in calculations
-
     // State variables
     address public vault; // Address of the associated vault contract
+    address public signer; // Address of the signer
     address public coldWallet; // Address of the cold wallet for fund storage
-    uint256 public startEpochTimestamp; // Timestamp of the start of the epoch
-    uint256 public epochPeriod; // Duration of each epoch
-    uint256 public withdrawalDelayTime; // Delay time for withdrawals
     mapping(address => uint256) public pairId; // token => pairId
 
     // Mappings
     mapping(address => bool) public isLPProvider; // Tracks whether an address is an LP provider
     mapping(address => uint256) public lpProvidedAmount; // Amount of liquidity provided by each LP provider
     mapping(address => mapping(address => uint256)) public claimableAmount; // after withdraw, user can claim profit,user => token => amount
+    mapping(bytes => bool) private _signatureUsed; // Tracks used signatures
 
-    // Structs
-    struct ReqWithdraw {
-        uint256 navAmount; // Amount of NAVs requested for withdrawal
-        uint256 timestamp; // Timestamp when the withdrawal can be executed
-    }
-
-    // Add this struct for EIP712 signature verification
-    struct WithdrawRequest {
-        uint256 requestId;
-        address user;
-        address token;
-        uint256 amount;
-        uint256 deadline;
-    }
-
-    // Add EIP712 domain separator and type hash constants
     bytes32 public constant WITHDRAW_TYPEHASH = keccak256(
-        "WithdrawRequest(uint256 requestId,address user,address token,uint256 amount,uint256 deadline)"
+        "WithdrawRequest(uint256 requestId,address user,address token,uint256 amount)"
     );
     bytes32 public immutable DOMAIN_SEPARATOR;
 
@@ -77,24 +54,13 @@ contract LpProvider is OwnableUpgradeable, ReentrancyGuardUpgradeable, EIP712Upg
     event WithdrawFund(
         address indexed user,
         address indexed token,
-        uint256 amount
-    );
-    event WithdrawRequested(
-        address indexed user,
-        address indexed token,
-        uint256 navAmount,
-        uint256 timestamp
+        uint256 amount,
+        uint256 reqId
     );
     event LPProviderStatusChanged(address indexed lpProvider, bool isProvider);
     event VaultChanged(address indexed newVault);
     event ColdWalletChanged(address indexed newColdWallet);
-    event EpochParametersChanged(
-        uint256 newStartEpochTimestamp,
-        uint256 newEpochPeriod
-    );
-    event WithdrawalDelayTimeChanged(uint256 newWithdrawalDelayTime);
     event RewardDepositedForMarketMaker(address indexed token, uint256 amount);
-    event NAVPriceUpdated(address indexed token, uint256 newPrice);
 
     // Modifiers
     modifier onlyVault() {
@@ -106,34 +72,18 @@ contract LpProvider is OwnableUpgradeable, ReentrancyGuardUpgradeable, EIP712Upg
     function initialize(
         address _owner,
         address _vault,
-        uint256 _epochPeriod,
-        uint256 _startEpochTimestamp,
-        uint256 _withdrawalDelayTime,
         address _coldWallet
     ) public initializer {
         __Ownable_init(_owner);
+        __EIP712_init("VDEXLP", "1.0.0");
         __ReentrancyGuard_init();
         vault = _vault;
-        epochPeriod = _epochPeriod;
-        startEpochTimestamp = _startEpochTimestamp;
-        withdrawalDelayTime = _withdrawalDelayTime;
+        signer = _owner;
         coldWallet = _coldWallet;
 
         // Emit events for initial parameter settings
         emit VaultChanged(_vault);
-        emit EpochParametersChanged(_startEpochTimestamp, _epochPeriod);
-        emit WithdrawalDelayTimeChanged(_withdrawalDelayTime);
         emit ColdWalletChanged(_coldWallet);
-
-        DOMAIN_SEPARATOR = keccak256(
-            abi.encode(
-                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256("LpProvider"),
-                keccak256("1"),
-                block.chainid,
-                address(this)
-            )
-        );
     }
 
     // External functions
@@ -144,7 +94,6 @@ contract LpProvider is OwnableUpgradeable, ReentrancyGuardUpgradeable, EIP712Upg
      * @param amount The amount of tokens to deposit
      */
     function depositFund(address token, uint256 amount) external nonReentrant {
-        require(isLPProvider[msg.sender], "Not LP provider");
         require(amount > 0, "Amount must be greater than zero");
         require(IVault(vault).isTokenSupported(token), "Token not supported");
 
@@ -155,48 +104,30 @@ contract LpProvider is OwnableUpgradeable, ReentrancyGuardUpgradeable, EIP712Upg
 
         emit DepositFund(msg.sender, token, amount);
     }
-
-    /**
+/**
      * @dev Withdraws funds with signature verification
-     * @param requestId Unique identifier for the withdrawal request
      * @param token The token to withdraw
      * @param amount The amount to withdraw
-     * @param deadline Timestamp after which the signature is invalid
+     * @param requestId Unique identifier for the withdrawal request
      * @param signature EIP712 signature from backend
      */
     function withdrawFund(
-        uint256 requestId,
         address token,
         uint256 amount,
-        uint256 deadline,
+        uint256 requestId,
         bytes calldata signature
     ) external nonReentrant {
-        require(isLPProvider[msg.sender], "Not LP provider");
         require(amount > 0, "Amount must be greater than zero");
 
         // Verify signature
-        bytes32 structHash = keccak256(
-            abi.encode(
-                WITHDRAW_TYPEHASH,
-                requestId,
-                msg.sender,
-                token,
-                amount,
-                deadline
-            )
-        );
-        bytes32 hash = keccak256(
-            abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, structHash)
-        );
-        address signer = ECDSAUpgradeable.recover(hash, signature);
-        require(signer == owner(), "Invalid signature");
+        _verifyWithdrawProof(msg.sender, token, amount, requestId, signature);
 
         require(
             IERC20(token).transfer(msg.sender, amount),
             "Transfer failed"
         );
 
-        emit WithdrawFund(msg.sender, token, amount);
+        emit WithdrawFund(msg.sender, token, amount, requestId);
     }
 
     /**
@@ -264,23 +195,7 @@ contract LpProvider is OwnableUpgradeable, ReentrancyGuardUpgradeable, EIP712Upg
     ) external onlyVault {
         claimableAmount[user][token] += amount;
     }
-
-    /**
-     * @dev Allows the vault to claim profits for a specific token
-     * @param token The address of the token
-     * @param amount The amount of tokens to claim as profit
-     */
-    function claimProfit(
-        address token,
-        uint256 amount
-    ) external onlyVault {
-        require(IERC20(token).transfer(vault, amount), "Transfer failed");
-        lpProvidedAmount[token] -= amount;
-        claimableAmount[msg.sender][token] -= amount;
-        emit LPWithdrawn(address(this), token, amount);
-    }
-
-    // Owner-only functions
+// Owner-only functions
 
     /**
      * @dev Sets the LP provider status for multiple addresses
@@ -296,6 +211,14 @@ contract LpProvider is OwnableUpgradeable, ReentrancyGuardUpgradeable, EIP712Upg
             isLPProvider[lpProvider[i]] = isProvider[i];
             emit LPProviderStatusChanged(lpProvider[i], isProvider[i]);
         }
+    }
+
+    /**
+     * @dev Sets the signer address
+     * @param _signer The new signer address
+     */
+    function setSigner(address _signer) external onlyOwner {
+        signer = _signer;
     }
 
     /**
@@ -333,27 +256,37 @@ contract LpProvider is OwnableUpgradeable, ReentrancyGuardUpgradeable, EIP712Upg
         }
     }
 
-    /**
-     * @dev Sets the epoch parameters
-     * @param _startEpochTimestamp The new start epoch timestamp
-     * @param _epochPeriod The new epoch period
-     */
-    function setEpochParameters(
-        uint256 _startEpochTimestamp,
-        uint256 _epochPeriod
-    ) external onlyOwner {
-        startEpochTimestamp = _startEpochTimestamp;
-        epochPeriod = _epochPeriod;
-        emit EpochParametersChanged(_startEpochTimestamp, _epochPeriod);
-    }
-
-     function _withdrawHash(
+    function _withdrawHash(
         address _user,
+        address _token,
         uint256 _amount,
-        uint256 _nonce
+        uint256 _reqId
     ) private view returns (bytes32 hash) {
         hash = _hashTypedDataV4(
-            keccak256(abi.encode(CLAIM_TYPEHASH, _user, _amount, _nonce))
+            keccak256(abi.encode(WITHDRAW_TYPEHASH, _reqId, _user, _token, _amount))
         );
+    }
+
+    function _verifyWithdrawProof(
+        address _user,
+        address _token,
+        uint256 _amount,
+        uint256 _reqId,
+        bytes memory _signature
+    ) private {
+        require(!_signatureUsed[_signature], "Signature already used");
+        require(
+            _verify(_withdrawHash(_user, _token, _amount, _reqId), _signature),
+            "Not approval by system!"
+        );
+        _signatureUsed[_signature] = true;
+    }
+
+    function _verify(bytes32 _digest, bytes memory _signature)
+        private
+        view
+        returns (bool)
+    {
+        return signer == ECDSA.recover(_digest, _signature);
     }
 }
